fc484193a08c45c57de817dfbc2c2eb0f8463ecb
berkanbugday <berkan.bugday92@gmail.com>
2025-03-08 16:15:46 +0300

    chore: centralize Jest configuration in config package

---
+++ packages/config/jest/mobile.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|@react-navigation)/)'
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js'
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
+  cacheDirectory: '.jest/cache'
+}; 
