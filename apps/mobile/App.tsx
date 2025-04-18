b25617820c26d37928c95474aba3b097dc5847a3
berkanbugday <berkan.bugday92@gmail.com>
2025-04-18 11:45:50 +0300

    refactor: simplify App component structure and remove unused navigation components; migrate to ES module syntax in configuration files and update package.json dependencies

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
-import {TabNavigator} from './src/navigation/TabNavigator';
 
 function App(): React.ReactElement {
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
+  return <SafeAreaProvider></SafeAreaProvider>;
 }
 
 export default App;
--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,8 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const path = require('path');
+import {getDefaultConfig, mergeConfig} from '@react-native/metro-config';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /**
  * Metro configuration
@@ -19,4 +22,4 @@ const config = {
   },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+export default mergeConfig(getDefaultConfig(__dirname), config);
